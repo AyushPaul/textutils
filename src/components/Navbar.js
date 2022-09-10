5c328d67713a00fcc27a617fd08478146e9c9805
Ayush Paul <ayushpaul2002@gmail.com>
2022-09-10 20:40:06 +0530

    added text area

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,4 +27,5 @@ export default function Navbar(props) {
   )
 }
 
-Navbar.PropTypes = {title : PropTypes.string , aboutText:PropTypes.string} 
+Navbar.propTypes = {title : PropTypes.string.isRequired , aboutText:PropTypes.string.isRequired} 
+Navbar.defaultProps = {title:"Set Title Here" , aboutText:"Set About Here"}
